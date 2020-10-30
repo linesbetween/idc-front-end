d41b881788e7a6a4e26d8aa8df54d2aa768ed4fc
lines <linesbetween@users.noreply.github.com>
2020-10-30 01:49:17 -0400

    configure enviroment for deploy

--- src/app/shared/search.service.ts
+++ src/app/shared/search.service.ts
@@ -1,18 +1,18 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  SERVER_ULR: string = 'http://localhost:8180/query';
 
   constructor(private httpeClient: HttpClient) {
   }
 
   public search(terms: string): Observable<{result: string}> {
     return this.httpeClient.post<{result: string}>(
-      `${this.SERVER_ULR}`, terms);
+      `${environment.API_URL}/query`, terms);
   }
 }
--- src/app/shared/upload.service.ts
+++ src/app/shared/upload.service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
-  SERVER_ULR: string = 'http://localhost:8180/upload';
 
   constructor(private httpeClient: HttpClient) {
   }
 
   public upload(formData, colorCode) {
-    return this.httpeClient.post<any>(`${this.SERVER_ULR}/${colorCode}`, formData, {
+    return this.httpeClient.post<any>(`${environment.API_URL}/upload/${colorCode}`, formData, {
       reportProgress: true,
       observe: 'events'
     });
